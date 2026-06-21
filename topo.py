d8a9224937457d706fe3a5dff52634f5126c71b9
gonzadom <97991084+gonzadom@users.noreply.github.com>
2026-06-21 01:30:54 -0300

    Agrego hosts 2 host mas a la topologia

--- topo.py
+++ topo.py
@@ -37,9 +37,17 @@ class NATTopo(Topo):
 
         h2 = self.addHost('h2', ip='192.168.1.2/24', mac='00:00:00:00:00:02',
                           defaultRoute='via 192.168.1.254')
+        
+        h3 = self.addHost('h3', ip='192.168.1.3/24', mac='00:00:00:00:00:03',
+                          defaultRoute='via 192.168.1.254')
+        
+        h4 = self.addHost('h4', ip='192.168.1.4/24', mac='00:00:00:00:00:04',
+                          defaultRoute='via 192.168.1.254')
 
         self.addLink(h1, s1)
         self.addLink(h2, s1)
+        self.addLink(h3, s1)
+        self.addLink(h4, s1)
 
 
 def run():
